814bdf9b5e5d98acccbd195a3585891efba0fa20
Riley Hedges <rhedges1@usf.edu>
2025-03-25 16:00:09 -0400

    inClass

---
+++ js_04.js
@@ -0,0 +1,15 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+export async function getPosts(params) {
+    try { 
+        const res = await fetch(BASE_URL);
+
+        if (!res.ok) {
+            throw new Error(`Error: ${res.status}`);
+        }
+        return await res.json();
+    } catch (err) {
+        console.error("API Fetch Failed:", err.message);
+        throw err;
+    }
+}
