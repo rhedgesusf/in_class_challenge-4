3bea17a98d2b5394c83aeb9ef10e3b3b33b5b99f
Riley Hedges <rhedges1@usf.edu>
2025-03-25 16:28:52 -0400

    Fixed

--- js_04.js
+++ js_04.js
@@ -1,24 +1,6 @@
-const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
-
-export async function getPosts(params) {
-    try { 
-        const res = await fetch(BASE_URL);
-
-        if (!res.ok) {
-            throw new Error(`Error: ${res.status}`);
-        }
-        return await res.json();
-    } catch (err) {
-        console.error("API Fetch Failed:", err.message);
-        throw err;
-    }
-}
-
-// part 3
-
 import { getPosts } from "./api.js"
 
-asynch function displayPosts() {
+async function displayPosts() {
     const container = document.getElemenyByID("postContainer");
  
     try { 
@@ -26,7 +8,8 @@ asynch function displayPosts() {
 
         posts.slice(0, 5).forEacH(post => { 
             const div = document.createElement('div');
-            div.innterHTML = `<h3>${post.title}</h3><p>${post.body}</p>`
+            div.innterHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
+            container.appendChild(div);
         });
     } catch (err) {
         container.innterHTML = `<p style="color:red;">Failed to load posts:${err.message}</p>`;       
